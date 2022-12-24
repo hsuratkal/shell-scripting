b60567095ef0960cc01c578e01f3d9b93f02845f
CodingManoj <mvp2612@hotmail.com>
2022-12-24 07:14:37 +0530

    enabling the esc seq

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -3,6 +3,11 @@
 # DATE="24-12-2022"   # HardCoding the value
 DATE=$(date +%F)      # Deeclaring expression to the variab;e
 echo "Good Morning, today's date is $DATE"  
+echo "Total number of opened sessions is $(who | wc -l)"
 
 
 # Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
+
+# () : Paranthesis      : Expressions should be enclused in paranthesis 
+# {} : Flower Brackets
+# [] : Square Brackets 
