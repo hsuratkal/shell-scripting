dc7ce46443e1c6602ec1f1d95a495cdfff40b037
CodingManoj <mvp2612@hotmail.com>
2022-12-24 06:38:56 +0530

    enabling the esc seq

--- 03-colors.sh
+++ 03-colors.sh
@@ -1,7 +1,15 @@
 #!/bin/bash 
 
+
+# Once you enable the color, ensure you close the color with \e[0m, if not color won't end
+
+# Prining Foreground text
 echo -e "\e[32m I am printing Green Color \e[0m"
 echo -e "\e[31m I am printing Red Color \e[0m"
 echo -e "\e[33m I am printing Yellow Color \e[0m"
 echo -e "\e[34m I am printing Blue Color \e[0m"
 echo -e "\e[35m I am printing Cyan Color \e[0m"
+
+# How to print background color ?
+
+echo -e "\e[41;33m Welcome to colors \e[0m "
