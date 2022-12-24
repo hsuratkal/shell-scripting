8763f6466785dbbeb9e3819917610df300962ea6
CodingManoj <mvp2612@hotmail.com>
2022-12-24 07:19:44 +0530

    enabling the esc seq

---
+++ 06-special-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+
+# Special Variable are pre-defined, which means you cannot create them, you can only use them.
+# Here are few of the special variables.
+
+###   $0 to $n , $*  $& $#  $? 
+
+# $0       : Prints the script name 
+# $?       : Shows the exit code of the previos command  
+# $0 to $9 : command line variables 
+
+echo "Name of the script is : $0 "
