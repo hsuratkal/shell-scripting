127dc2900fdfae217ff944a6bb3cbaaca9859a5a
CodingManoj <mvp2612@hotmail.com>
2022-12-24 07:16:20 +0530

    enabling the esc seq

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -2,7 +2,7 @@
 
 # DATE="24-12-2022"   # HardCoding the value
 DATE=$(date +%F)      # Deeclaring expression to the variab;e
-echo "Good Morning, today's date i \e[32m  $DATE  \e[0m"  
+echo -e "Good Morning, today's date i \e[32m  $DATE  \e[0m"  
 echo -e "Total number of opened sessions is \e[32m $(who | wc -l) \e[0m"
 
 
