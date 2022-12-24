13bf7f0e37e8624afef9b9190eb3cbda7665f2c6
CodingManoj <mvp2612@hotmail.com>
2022-12-24 06:36:19 +0530

    enabling the esc seq

--- 03-colors.sh
+++ 03-colors.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 
-echo -e "\e[32m I am printing Green Colot \e[0m"
+echo -e "\e[32m I am printing Green Color \e[0m"
+echo -e "\e[31m I am printing Red Color \e[0m"
+echo -e "\e[33m I am printing Yellow Color \e[0m"
+echo -e "\e[34m I am printing Blue Color \e[0m"
+echo -e "\e[35m I am printing Cyan Color \e[0m"
