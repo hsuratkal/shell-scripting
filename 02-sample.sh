4829baa2684fdf258abc8d7fdedd5136c72c0695
CodingManoj <mvp2612@hotmail.com>
2022-12-24 06:24:19 +0530

    Added agenda file

--- 02-sample.sh
+++ 02-sample.sh
@@ -4,6 +4,11 @@
 
 echo Hello World
 
-echo Line1 
+# Escape Sequence Characters will be considered by echo command only if we enable it by using the -e option 
+
+# \n : New Line 
+# \t : Tab Space
+
+echo Line1\nLine2
 echo Line2
 echo Line3
