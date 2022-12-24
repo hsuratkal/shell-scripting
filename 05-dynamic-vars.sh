d66be8a8a6de16dd48fc2f34687fc5dd5dd235cb
CodingManoj <mvp2612@hotmail.com>
2022-12-24 07:08:54 +0530

    enabling the esc seq

--- 05-dynamic-vars.sh
+++ 05-dynamic-vars.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
 
-DATE="24-12-2022"
+# DATE="24-12-2022"   # HardCoding the value
+DATE=$(date +%F)      # Deeclaring expression to the variab;e
 echo "Good Morning, today's date is $DATE"  
+
+
+# Always fetch the information dynamically for the variable which keep on changing, hardcoding creates complexity
