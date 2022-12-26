88d4f3a172ab86ac20383246508d14a2a8bf0ec8
CodingManoj <mvp2612@hotmail.com>
2022-12-26 07:06:27 +0530

    wefdsx

--- 08-function.sh
+++ 08-function.sh
@@ -2,14 +2,14 @@
 
 # Declaring a sample function 
 # ( paranthesis )
-sample() {
-    echo "I am a sample function"
-    echo " If you want to me call me, just type sample"
-    echo "sample function is completed"
-}
+# sample() {
+#     echo "I am a sample function"
+#     echo " If you want to me call me, just type sample"
+#     echo "sample function is completed"
+# }
 
-# calling the function
-sample 
+# # calling the function
+# sample 
 
 stat() {
     echo "Number of opened sessions : $(who | wc -l)" 
