bc44756dad6d272482496594fc016045714fb06e
CodingManoj <mvp2612@hotmail.com>
2022-12-27 06:46:14 +0530

    wefdsx

--- 13-loops.sh
+++ 13-loops.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
-for list in val1 val2 val3 val4 val5 ; do  
-    echo Value is $list
+# for list in val1 val2 val3 val4 val5 ; do  
+#     echo Value is $list
+# done 
+
+for courses in devops aws gcp azure terraform ansible docker ; do 
+    echo course name is $courses
 done 
