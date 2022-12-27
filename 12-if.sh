ada89abf927700fe829030a1c29f35bf82b0d285
CodingManoj <mvp2612@hotmail.com>
2022-12-27 06:29:00 +0530

    wefdsx

--- 12-if.sh
+++ 12-if.sh
@@ -2,7 +2,17 @@
 
 a="xyz"
 
+# If Condition
+# if [ "$a" == "xyz" ]; then 
+#     echo -e "\e[32m Both of the are equal \e[0m"
+
+# fi 
+
+# If else condition
+
 if [ "$a" == "xyz" ]; then 
     echo -e "\e[32m Both of the are equal \e[0m"
 
+else 
+    echo -e "\e[31m Both of the are not equal \e[0m"
 fi 
