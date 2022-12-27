23b8b7234d6a882e25d1e9e97be1f21cc7224717
CodingManoj <mvp2612@hotmail.com>
2022-12-27 07:16:14 +0530

    wefdsx

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -2,7 +2,7 @@
 
 UID=$(id -u)
 if [ $UID -ne 0 ] ; then 
-    echo -e "\e[32m You need to script either as a root user or with a sudo privilege \e[0m"
+    echo -e "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
     exit 1
 fi 
 
