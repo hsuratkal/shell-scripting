bee752b0afe19a56eb119e621bec9d3e01794060
CodingManoj <mvp2612@hotmail.com>
2022-12-27 07:15:34 +0530

    wefdsx

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash 
 
+UID=$(id -u)
+if [ $UID -ne 0 ] ; then 
+    echo -e "\32m You need to script either as a root user or with a sudo privilege \e[0m"
+    exit 1
+fi 
+
 yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
