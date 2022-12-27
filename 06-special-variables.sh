c43f1d1312c173fd5eb692ac450998b629c44135
CodingManoj <mvp2612@hotmail.com>
2022-12-27 06:11:20 +0530

    wefdsx

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -7,10 +7,11 @@
 ###   $0 to $n , $*  $& $#  $? 
 
 # $0       : Prints the script name 
-# $?       : Shows the exit code of the previos command  
+# $?       : Shows the exit code of the previous command  
 # $1 to $9 : command line variables ; ex : sh test.sh  100 200 300 
 # $* or $@ : Prints all the variables used in this script
 # $#       : Prints the number variables used in this script
+# In linux apart from A to Z or a to z or 0 to  9 are special characters 
 
 a=$1 
 b=$2 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # shell-scripting
 
 This is a repository which contains all the shell basics and the project automation scripts.
+
+# Start from Exit codes
