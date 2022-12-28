b08f2473a927269ad78420b5d03eac3d15697832
CodingManoj <mvp2612@hotmail.com>
2022-12-28 07:01:03 +0530

    wefdsx

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -6,12 +6,12 @@ COMPONENT=catalogue
 source robot/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Configuring NodeJS Repo :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE 
 yum install nodejs -y  &>> $LOGFILE 
 stat $? 
 
 echo -n "Creating Application User $APPUSER"
-useradd $APPUSER 
+useradd $APPUSER  &>> $LOGFILE 
 stat $? 
 
 
