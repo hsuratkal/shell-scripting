23c788aa7e482d6b2f0ce3746515f7772878049a
CodingManoj <mvp2612@hotmail.com>
2022-12-28 06:43:30 +0530

    wefdsx

--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -15,9 +15,10 @@ stat() {
         echo -e "\e[32m Success \e[0m "
     else 
         echo -e "\e[31m failure \e[0m"
+    fi 
 }
 
-echo -n "Donwloading $COMPONENT : "
+echo -n "Downloading $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $? 
 
