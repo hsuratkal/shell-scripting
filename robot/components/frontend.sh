96c6fa78dc476c40daf2adad8a311f5da21f89ba
CodingManoj <mvp2612@hotmail.com>
2022-12-28 22:21:16 +0530

    Auto Push Commit

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -3,7 +3,7 @@
 set -e
 
 COMPONENT=frontend
-LOFGILE=/tmp/$COMPONENT.log
+LOFGILE=/tmp/$COMPONENT
 
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
