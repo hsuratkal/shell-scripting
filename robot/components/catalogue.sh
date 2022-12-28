5a924221f6dd47bfaab9f8a6b404848259250d99
CodingManoj <mvp2612@hotmail.com>
2022-12-28 22:08:55 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -7,8 +7,8 @@ source components/common.sh    # Source loads a file and this file has all the c
 
 
 echo -n "Configuring Node JS:"
-    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -   &>> $LOGFILE
-    stat $? 
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -   &>> $LOGFILE
+stat $? 
 echo -n "Configuring NodeJS Repo :"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 yum install nodejs -y  &>> $LOGFILE 
