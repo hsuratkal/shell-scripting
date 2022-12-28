80eb1dd2c57043bf8f2f184072381ec8095e3593
CodingManoj <mvp2612@hotmail.com>
2022-12-28 22:22:25 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -7,7 +7,7 @@ LOFGILE=/tmp/$COMPONENT
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Configuring Node JS:"
-curl -sL https://rpm.nodesource.com/setup_16.x | bash  
+curl -sL https://rpm.nodesource.com/setup_16.x | bash  &>>
 stat $? 
 echo -n "Configuring NodeJS Repo :"
 
