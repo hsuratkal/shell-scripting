79d9b8b352748da956d77de62294edb39c9abbd1
CodingManoj <mvp2612@hotmail.com>
2022-12-28 22:09:00 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -10,9 +10,6 @@ echo -n "Configuring Node JS:"
 curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -   &>> $LOGFILE
 stat $? 
 echo -n "Configuring NodeJS Repo :"
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
-yum install nodejs -y  &>> $LOGFILE 
-stat $? 
 
 echo -n "Creating Application User $APPUSER"
 useradd $APPUSER  &>> $LOGFILE 
