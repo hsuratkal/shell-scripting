e6eb7328c5412c6a0d317017b02507fe284f738f
CodingManoj <mvp2612@hotmail.com>
2022-12-28 22:33:14 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -8,7 +8,6 @@ source components/common.sh    # Source loads a file and this file has all the c
 echo -n "Configuring Node JS:"
 curl -sL https://rpm.nodesource.com/setup_16.x | bash  &>> ${LOFGILE}
 stat $? 
-echo -n "Configuring NodeJS Repo :"
 
 echo -n "Creating Application User $APPUSER"
 useradd $APPUSER  &>> "${LOFGILE}"
