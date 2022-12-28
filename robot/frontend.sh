d269e3ea2f6f9cdb9265c1975010a926d1791edb
CodingManoj <mvp2612@hotmail.com>
2022-12-28 06:22:01 +0530

    wefdsx

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,25 +1,41 @@
 #!/bin/bash 
 
+COMPONENT=frontend
+
 ID=$(id -u)
 if [ $ID -ne 0 ] ; then 
     echo -e "\e[31m You need to script either as a root user or with a sudo privilege \e[0m"
     exit 1
 fi 
 
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m Success \e[0m "
+    else 
+        echo "\e[31m failure \e[0m"
+    fi 
+}
+
 echo -n "Installing Nginx :"
 yum install nginx -y    &>> /tmp/frontend.log 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m "
-else 
-    echo "\e[31m failure \e[0m"
-fi 
+stat $? 
 
 
 echo -n "Starting Nignx :"
 systemctl enable nginx  &>> /tmp/frontend.log 
 systemctl start nginx   &>> /tmp/frontend.log 
-if [ $? -eq 0 ]; then 
-    echo -e "\e[32m Success \e[0m "
-else 
-    echo "\e[32m failure \e[0m"
-fi 
+stat $?
+
+
+echo -n "Downloading the $COMPONENT "
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $?
+
+
+# cd /usr/share/nginx/html
+# rm -rf *
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-main README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
