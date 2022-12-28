55c189c6976bab30eaac27fe9bac4e0fda936b1c
CodingManoj <mvp2612@hotmail.com>
2022-12-28 06:24:55 +0530

    wefdsx

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -20,22 +20,28 @@ echo -n "Installing Nginx :"
 yum install nginx -y    &>> /tmp/frontend.log 
 stat $? 
 
-
-echo -n "Starting Nignx :"
-systemctl enable nginx  &>> /tmp/frontend.log 
-systemctl start nginx   &>> /tmp/frontend.log 
-stat $?
-
-
-echo -n "Downloading the $COMPONENT "
+echo -n "Downloading the $COMPONENT :"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
 stat $?
 
 
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -n "Clearing the default content : "
+cd /usr/share/nginx/html
+rm -rf *   &>> /tmp/frontend.log 
+stat $? 
+
+echo -n "Extracting $COMPONENT : "
+unzip /tmp/COMPONENT.zip &>> /tmp/frontend.log 
+stat $? 
+
+echo -n "Copying $COMPONENT :"
+mv frontend-main/* .  &>> /tmp/frontend.log 
+mv static/* .         &>> /tmp/frontend.log 
+rm -rf frontend-main README.md  &>> /tmp/frontend.log 
+mv localhost.conf /etc/nginx/default.d/roboshop.conf  &>> /tmp/frontend.log 
+stat $? 
+
+echo -n "Retarting Nignx :"
+systemctl enable nginx  &>> /tmp/frontend.log 
+systemctl restart nginx   &>> /tmp/frontend.log 
+stat $?
