1b7930d7d61eed6b273db77f902df1eb43022049
CodingManoj <mvp2612@hotmail.com>
2022-12-29 06:41:10 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -18,13 +18,12 @@ echo -n "Downloading the $COMPONENT :"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
 stat $? 
 
-echo -n "Cleanup and Extracting the $COMPONENT: "
+echo -n "Cleanup and Extraction $COMPONENT: "
 cd /home/$APPUSER
 unzip -o /tmp/$COMPONENT.zip  &>> "${LOFGILE}"
 stat $? 
 
-echo -n "Performing cleanup : "
-rm -rf /home/$APPUSER/$COMPONENT/
+
 
 echo -n "Changing the ownership to $APPUSER"
 mv /home/$APPUSER/$COMPONENT-main /home/$APPUSER/$COMPONENT
