a4fd1d1d43ce0be164986be829b779f20ec1f009
CodingManoj <mvp2612@hotmail.com>
2022-12-29 07:07:48 +0530

    Auto Push Commit

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -6,4 +6,7 @@ source components/common.sh    # Source loads a file and this file has all the c
 
 echo -n "Configuring the $COMPONENT repo  : "
 curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+
+echo -n "Intsalling $COMPONENT :"
 yum install redis-6.2.7 -y &>> "${LOFGILE}"
+stat $? 
