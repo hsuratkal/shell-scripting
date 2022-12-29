556b3c2bcbf830dd85a43185e2709bcb7ae56db4
CodingManoj <mvp2612@hotmail.com>
2022-12-29 07:08:22 +0530

    Auto Push Commit

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -13,7 +13,7 @@ yum install redis-6.2.7 -y &>> "${LOFGILE}"
 stat $? 
 
 echo -n "whitelisting the $COMPONENT"
-sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 stat $?
 
 echo -n "Starting $COMPONENT : "
