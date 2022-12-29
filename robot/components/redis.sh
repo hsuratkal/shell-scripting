0da7f6e3f303eae52f5e64b843428a23a7d5dcf3
CodingManoj <mvp2612@hotmail.com>
2022-12-29 07:13:57 +0530

    Auto Push Commit

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -13,7 +13,7 @@ yum install redis-6.2.7 -y &>> "${LOFGILE}"
 stat $? 
 
 echo -n "whitelisting the $COMPONENT"
-sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e  's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
 sed -i -e  's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
 stat $?
 
