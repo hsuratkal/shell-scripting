fcf39b1d579377a37d3bcbb0852d92b2c11b7f56
CodingManoj <mvp2612@hotmail.com>
2022-12-29 06:58:31 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -47,6 +47,7 @@ echo -n "Starting $COMPONENT Service :"
 systemctl daemon-reload &>> "${LOFGILE}"
 systemctl enable catalogue &>> "${LOFGILE}"
 systemctl restart catalogue &>> "${LOFGILE}"
+systemctl restart catalogue &>> "${LOFGILE}"
 stat $? 
 
  echo -e "\e[32m ______ $COMPONENT Configuration Completed _________ \e[0m"
