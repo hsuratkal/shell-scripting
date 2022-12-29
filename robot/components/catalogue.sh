a31e9100c9e1367eab4400a2355d740daac38b23
CodingManoj <mvp2612@hotmail.com>
2022-12-29 07:38:32 +0530

    Auto Push Commit

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -12,16 +12,3 @@ NODEJS                         # Calling NodeJS Function
 
 
 
-echo -n "Configuring the $COMPONENT Service:"
-sed -i -e  's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service  /etc/systemd/system/$COMPONENT.service 
-stat $? 
-
-echo -n "Starting $COMPONENT Service :"
-systemctl daemon-reload &>> "${LOFGILE}"
-systemctl enable $COMPONENT &>> "${LOFGILE}"
-systemctl restart $COMPONENT &>> "${LOFGILE}" 
-systemctl status $COMPONENT &>> "${LOFGILE}"
-stat $? 
-
- echo -e "\e[32m ______ $COMPONENT Configuration Completed _________ \e[0m"
