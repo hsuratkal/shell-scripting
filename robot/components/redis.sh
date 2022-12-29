d2683855a6076b4d0ff4c6643b65f8401c075b4e
CodingManoj <mvp2612@hotmail.com>
2022-12-29 07:06:32 +0530

    Auto Push Commit

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -5,5 +5,4 @@ COMPONENT=mongodb
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Downloading $COMPONENT : "
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-stat $? 
+
