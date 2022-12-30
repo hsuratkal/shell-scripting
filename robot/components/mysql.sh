cb9fc6e6f521e7eb9c8fcb7a9330fc09c670e554
CodingManoj <mvp2612@hotmail.com>
2022-12-30 06:19:36 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -5,11 +5,11 @@ COMPONENT=mysql
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
 echo -n "Configuring the $COMPONENT repo  : "
-curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/$COMPONENT/main/$COMPONENT.repo  &>> "${LOFGILE}"
 stat $? 
 
 echo -n "Installing $COMPONENT: "
-yum install mysql-community-server -y 
+yum install mysql-community-server -y   &>> "${LOFGILE}"
 
 
 # 1. Install MySQL
