8e1f79a06b5167191fcf431e8b35cc09dbdf8d9d
CodingManoj <mvp2612@hotmail.com>
2022-12-30 06:17:23 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -7,3 +7,52 @@ source components/common.sh    # Source loads a file and this file has all the c
 echo -n "Downloading $COMPONENT : "
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $? 
+
+
+MySQL is the database service that is needed for the application. So we need to install it and configure it for the application to work.
+
+## **Manual Steps to Install MySQL**
+
+As per the Application need, we are choosing MySQL 5.7 version.
+
+1. Setup MySQL Repo
+
+```bash
+# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+```
+
+1. Install MySQL
+
+```bash
+# yum install mysql-community-server -y
+```
+
+1. Start MySQL.
+
+```bash
+# systemctl enable mysqld 
+# systemctl start mysqld
+```
+
+1. Now a default root password will be generated and can be seen in the log file.
+
+```bash
+# grep temp /var/log/mysqld.log
+( Copy that password )
+```
+
+1. Next, We need to change the default root password in order to start using the database service. Use password as `RoboShop@1` . Rest of the options you can choose `No`
+
+```bash
+# mysql_secure_installation
+```
+
+1. You can check whether the new password is working or not using the following command in MySQL
+
+First let's connect to MySQL
+
+```bash
+# mysql -uroot -pRoboShop@1
+```
+
+Once after login to MySQL prompt then run this SQL Command. This will uninstall the password validation feature like number of characters, password length, complexty and all. As I don’t want that I’d be uninstalling the `validate_password` plugin
