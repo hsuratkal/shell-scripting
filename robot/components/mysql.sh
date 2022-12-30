861b210a515da3f811cad7a887900aa826856d70
CodingManoj <mvp2612@hotmail.com>
2022-12-30 06:51:33 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -33,7 +33,7 @@ fi
 echo "show plugins | mysql -uroot -pRoboShop@1 | grep validate_password;   &>> "${LOFGILE}"
 if [ $? -eq 0 ]; then 
     echo -n "Uninstalling Password Validate Plugin "
-    echo " uninstall plugin validate_password;"  | mysql -uroot -pRoboShop@1 &>> "${LOFGILE}"
+    echo "show plugins;"|  mysql -uroot -pRoboShop@1 | grep validate_password
     stat $?
 fi 
 
