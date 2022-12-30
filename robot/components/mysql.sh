fddacd232c8ec643e8df6517ed724dd90337b0b3
CodingManoj <mvp2612@hotmail.com>
2022-12-30 06:17:50 +0530

    Auto Push Commit

--- robot/components/mysql.sh
+++ robot/components/mysql.sh
@@ -4,71 +4,73 @@ COMPONENT=mysql
 
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
-echo -n "Downloading $COMPONENT : "
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
-stat $? 
 
 
-MySQL is the database service that is needed for the application. So we need to install it and configure it for the application to work.
+# echo -n "Downloading $COMPONENT : "
+# curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+# stat $? 
 
-## **Manual Steps to Install MySQL**
 
-As per the Application need, we are choosing MySQL 5.7 version.
+# MySQL is the database service that is needed for the application. So we need to install it and configure it for the application to work.
 
-1. Setup MySQL Repo
+# ## **Manual Steps to Install MySQL**
 
-```bash
-# curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
-```
+# As per the Application need, we are choosing MySQL 5.7 version.
 
-1. Install MySQL
+# 1. Setup MySQL Repo
 
-```bash
-# yum install mysql-community-server -y
-```
+# ```bash
+# # curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+# ```
 
-1. Start MySQL.
+# 1. Install MySQL
 
-```bash
-# systemctl enable mysqld 
-# systemctl start mysqld
-```
+# ```bash
+# # yum install mysql-community-server -y
+# ```
 
-1. Now a default root password will be generated and can be seen in the log file.
+# 1. Start MySQL.
 
-```bash
-# grep temp /var/log/mysqld.log
-( Copy that password )
-```
+# ```bash
+# # systemctl enable mysqld 
+# # systemctl start mysqld
+# ```
 
-1. Next, We need to change the default root password in order to start using the database service. Use password as `RoboShop@1` . Rest of the options you can choose `No`
+# 1. Now a default root password will be generated and can be seen in the log file.
 
-```bash
-# mysql_secure_installation
-```
+# ```bash
+# # grep temp /var/log/mysqld.log
+# ( Copy that password )
+# ```
 
-1. You can check whether the new password is working or not using the following command in MySQL
+# 1. Next, We need to change the default root password in order to start using the database service. Use password as `RoboShop@1` . Rest of the options you can choose `No`
 
-First let's connect to MySQL
+# ```bash
+# # mysql_secure_installation
+# ```
 
-```bash
-# mysql -uroot -pRoboShop@1
-```
+# 1. You can check whether the new password is working or not using the following command in MySQL
 
-Once after login to MySQL prompt then run this SQL Command. This will uninstall the password validation feature like number of characters, password length, complexty and all. As I don’t want that I’d be uninstalling the `validate_password` plugin
+# First let's connect to MySQL
 
-## **Setup Needed for Application.**
+# ```bash
+# # mysql -uroot -pRoboShop@1
+# ```
 
-As per the architecture diagram, MySQL is needed by
+# Once after login to MySQL prompt then run this SQL Command. This will uninstall the password validation feature like number of characters, password length, complexty and all. As I don’t want that I’d be uninstalling the `validate_password` plugin
 
-- Shipping Service
+# ## **Setup Needed for Application.**
 
-So we need to load that schema into the database, So those applications will detect them and run accordingly.
+# As per the architecture diagram, MySQL is needed by
 
-To download schema, Use the following command
+# - Shipping Service
 
-```bash
-# curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-```
+# So we need to load that schema into the database, So those applications will detect them and run accordingly.
 
-Load the schema for mysql. This file contains the list of COUNTRIES, CITIES and their PINCODES. This will be helpful in doing the shipping charges calculation which is based on the distance the product is shippied
+# To download schema, Use the following command
+
+# ```bash
+# # curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+# ```
+
+# Load the schema for mysql. This file contains the list of COUNTRIES, CITIES and their PINCODES. This will be helpful in doing the shipping charges calculation which is based on the distance the product is shippied
