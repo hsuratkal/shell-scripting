cde621a937474735253e1f80a7c935d599c164da
CodingManoj <mvp2612@hotmail.com>
2022-12-30 07:09:13 +0530

    Auto Push Commit

--- robot/components/shipping.sh
+++ robot/components/shipping.sh
@@ -3,4 +3,4 @@
 COMPONENT=shipping
 source components/common.sh    # Source loads a file and this file has all the common patterns.
 
-JAVA                         # Calling NodeJS Function
+JAVA                         # Calling JAVA Function
