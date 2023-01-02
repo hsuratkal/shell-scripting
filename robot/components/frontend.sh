ff3ac45ae8db7dc0fbe1d01480eb275dea95033b
CodingManoj <mvp2612@hotmail.com>
2023-01-02 07:07:43 +0530

    asdf

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -31,7 +31,7 @@ stat $?
 
 for component in catalogue cart user shipping payment; do     # for loop 
     echo -n "Updating the backend reverse proxy dns records:"
-    sed -i -e "/$component/s/localhost/$component.roboshop.internal"  /etc/nginx/default.d/roboshop.conf  
+    sed -i -e "/$component/s/localhost/$component.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf  
     stat $? 
 done 
 
